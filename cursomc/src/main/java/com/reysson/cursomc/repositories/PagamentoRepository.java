b49c27211a66b6563538f2857cbe4026058ec8b6
Reysson <reysson.joseph29@gmail.com>
2020-02-26 21:33:21 -0300

    Pedido, Pagamento, EstadoPagamento

---
+++ cursomc/src/main/java/com/reysson/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,11 @@
+package com.reysson.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.reysson.cursomc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer>{
+	
+}
---
+++ cursomc/src/main/java/com/reysson/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,11 @@
+package com.reysson.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.reysson.cursomc.domain.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
+
+}
